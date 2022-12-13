33dcf98c3f8f7338bb6907cca0d0032da17f66c4
Daphnetatertot <daphnetatertot@gmail.com>
2022-12-12 21:06:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+$('#timelist').click(event, function(){
+
+console.log(event, event.target)
+// targets parent element of the target of the click
+console.log(event.target.parentElement);
+
+}) 
+
